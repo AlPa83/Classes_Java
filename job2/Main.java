900fe28b2b0e7a31aab0ea4164944cb4f53aa1f1
AlPa83 <alina.pavlidi@laplateforme.io>
2025-11-14 11:55:30 +0100

    Exercices 1 et 2

--- job2/Main.java
+++ job2/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
         // Création des objets
         Personne p1 = new Personne();
-        p1.nom = "Alice";
+        p1.nom = "Monica";
         p1.age = 25;
 
         Personne p2 = new Personne();
-        p2.nom = "Bob";
+        p2.nom = "Bernard";
         p2.age = 30;
 
         Personne p3 = new Personne();
-        p3.nom = "Charlie";
+        p3.nom = "Jean Michel";
         p3.age = 22;
 
         // Appel de la méthode pour chaque personne
